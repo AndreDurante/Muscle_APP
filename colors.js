9ab7f90f0c3d34d1d3b4b9e7041a58dc4136ddcd
André Durante <38120639+AndreDurante@users.noreply.github.com>
2020-10-06 12:07:21 -0300

    Ícones, Descrição e Edit

---
+++ colors.js
@@ -0,0 +1,41 @@
+/*
+export const purple = '#924E7D'
+export const blue = '#4E5C92'
+export const deepPurple = '#66455C'
+export const gold = '#92844E'
+export const green = '#4E9263'
+*/
+export const white = '#FFF'
+
+export const purple = '#9966ff'
+export const blue = '#668cff'
+export const gold = '#ffff66'
+export const green = '#66ff8c'
+export const deepPurple = '#b366ff'
+
+export const lightBrown = '#b35900' //'#ffbf80'
+export const grayBrown = '#d6bfa9'
+export const darkGrayBrown = '#bd9875'
+export const goldBrown = '#ffff80' //'#ffbd80''#a9d6bf''#d6d6a9''#ffff80'
+export const lightGreen = '#a9d6a9' //'#bfff80'
+
+
+export const backgroundColorStatusBar = 'gray' //darkGrayBrown
+export const backGroundInput = 'white'
+export const colorInput = 'black'
+export const colorIcon = 'green'
+export const backGround = "#FFF"//'#E6E6E6'//grayBrown
+export const backGroundHeader = 'gray' //darkGrayBrown
+export const darkBackGround = 'gray' //darkGrayBrown
+export const detail = 'black'
+export const icon = 'black'
+export const title = 'blue' //lightGreen //lightBrown
+export const styleText = 'black'
+
+
+// COOLORS.CO - PALETTE 93
+export const darkBlue = '#072AC8'
+export const midBlue = '#1E96FC'
+export const lightBlue = '#A2D6F9'
+export const yellow = '#FCF300'
+export const orange = '#FFC600'
